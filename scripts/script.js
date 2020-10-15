d963f933964b9d6014081deecdffb6c559392ee2
--Explosion-- <explosion.scratch@gmail.com>
2020-10-15 22:40:52 +0000

    Yay! Dark mode is working!

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 function darkMode() {
 	element = document.querySelector("*");
-	element.classList.toggle('dark-mode');
-	window.localStorage.setItem('dark_mode', 'true')
+	window.localStorage.setItem('dark_mode', element.classList.toggle('dark-mode'))
 };
